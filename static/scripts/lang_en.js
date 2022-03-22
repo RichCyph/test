08f55b59a842ea9be62491e3de3593e49c726d66
RichCyph <100386752+RichCyph@users.noreply.github.com>
2022-03-22 20:20:23 +0100

    committing new

---
+++ static/scripts/lang_en.js
@@ -0,0 +1,36 @@
+
+//lang_en.js
+
+sprache = {
+
+	logo_title: "home",
+	
+	morgengruss: "good morning",
+	nachmittaggruss: "good afternoon",
+	abendgruss: "good evening",
+	nachtgruss: "good night",
+	zeit:"time",
+	
+	datum:"date",
+	
+	suche:"search",
+	
+	lesezeichen:"bookmarks",
+	arbeit:"work",
+	lernen:"learning",
+	geld: "money",
+	sozial:"social",
+	musik:"music",
+	spiele:"games",
+	
+	zeitgeber:"timer",
+	zeitgeber_start:"start",
+	zeitgeber_stopp:"stop",
+	zeitgeber_pause:"pause",
+	zeitgeber_bearbeiten:"edit",
+	
+	kryptowahrung:"coinbase" 
+	
+};
+
+
