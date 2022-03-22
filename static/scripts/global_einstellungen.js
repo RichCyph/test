d7c45d1b359ca5c79c0a925c02c8a2ea9088dc2a
RichCyph <100386752+RichCyph@users.noreply.github.com>
2022-03-22 19:00:23 +0100

    committing new

---
+++ static/scripts/global_einstellungen.js
@@ -0,0 +1,15 @@
+//Urh Global Variablen
+var global_benutzer_name = document.getElementById("name").innerText; //"{{user.name}}!";
+
+//begrussungen
+var global_morgengruss = document.getElementById("morning_greeting").innerText; //"{{user.morning_greeting}}";
+var global_nachmittaggruss = document.getElementById("afternoon_greeting").innerText; //"{{user.afternoon_greeting}}";
+var global_abendgruss = document.getElementById("evening_greeting").innerText; //"{{user.evening_greeting}}";
+var global_nachtgruss = document.getElementById("night_greeting").innerText; //"{{user.night_greeting}}";
+
+//Wetter Global Variablen
+var global_lat = parseInt(document.getElementById("latitude").innerText); //"{{user.latitude}}";
+var global_lon = parseInt(document.getElementById("longitude").innerText); //"{{user.longitude}}";
+var global_lande_texte = document.getElementById("country").innerText; //"{{user.country}}";
+
+var global_sprachen_auswahl = "de" //en, de
