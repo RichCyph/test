50004603933a4240f2976da7e6c0910e518fb3a1
RichCyph <100386752+RichCyph@users.noreply.github.com>
2022-03-23 17:20:21 +0100

    committing new

---
+++ static/scripts/lang_de.js
@@ -0,0 +1,36 @@
+
+
+//lang_de.js
+
+sprache = {
+
+	logo_title: "zuhaus",
+	
+	morgengruss: "guten morgen",
+	nachmittaggruss: "guten nachmittage",
+	abendgruss: "guten abend",
+	nachtgruss: "gute nacht",
+	zeit:"zeit",
+	
+	datum:"datum",
+	
+	suche:"suche",
+	
+	lesezeichen:"lesezeichen",
+	arbeit:"arbeit",
+	lernen:"lernen",
+	geld: "geld",
+	sozial:"sozial",
+	musik:"musik",
+	spiele:"spiele",
+	
+	zeitgeber:"zeitgeber",
+	zeitgeber_start:"start",
+	zeitgeber_stopp:"stopp",
+	zeitgeber_pause:"pause",
+	zeitgeber_bearbeiten:"bearbeiten",
+	
+	kryptowahrung:"kryptowahrung"
+	
+};
+
